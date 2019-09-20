cb19e5e362da049ea11df476397d93a9c5e07d7f
ThirdGhostHand <ThirdGhostHand@outlook.com>
2019-09-19 18:05:30 -0700

    second commit

---
+++ src/components/Landing.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../styles/Landing.css';
+
+function Landing() {
+  return (
+    <div className="wrapper">
+      <div className="single">
+        <div className="post">
+        </div>
+      </div>
+      <div className="double">
+        <div className="post">
+        <div>
+        <div className="post">
+        </div>
+        {/* <div className="single">
+        <div className="post">
+        </div>
+      </div>
+      <div className="double">
+        <div className="post">
+        <div>
+        <div className="post">
+        </div> */}
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
